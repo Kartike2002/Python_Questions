fb3f523c3092129272b9c24526d83fc717791590
aditya nigam <adityanigam!4569@gmail.com>
2025-08-15 13:17:20 +0530

    Initial commit

---
+++ Assig_06.py
@@ -0,0 +1,29 @@
+# Q1
+x = 5
+print(type(x))
+y = str(x)
+print(type(y))
+
+# Q2
+
+print(ord('m'))
+
+# Q3
+
+print(chr(100))
+
+# Q4
+
+x = "Good Moring"
+print(type(x))
+# print(type(int(x)))
+
+# str + int = error
+# int + int = no error
+
+
+# Q5
+
+x = 10
+y = bool(x)
+print(y)
