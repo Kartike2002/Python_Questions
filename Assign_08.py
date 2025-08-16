337d4635737e6e3c95ea47056fd4367aae2fa65d
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-16 09:45:30 +0530

    Add files via upload

---
+++ Assign_08.py
@@ -0,0 +1,27 @@
+#Q1
+x=input("Enter the your name")
+print(x)
+
+#Q2
+
+a=int(input("Enter the first num"))
+b=int(input("Enter the Second num"))
+c=a+b
+print("sum is ",c)
+
+#Q3
+
+r=float(input("Enter the radious of circle"))
+area=2*3.14*r
+print("radious of area is",area)
+
+#Q4
+
+n=int(input("Enter the number"))
+square=n**2
+print("square of num ",square)
+
+#Q5
+
+x=input("Enter the character")
+print(ord(x))
