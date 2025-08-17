93e1927f11c2896034a91d32c854047302a7e9b3
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-17 16:37:49 +0530

    Add files via upload

---
+++ Assign_09.py
@@ -0,0 +1,34 @@
+#Q1
+P=int(input("Enter the Principal"))
+R=int(input("Enter the Rate of Intrest"))
+T=int(input("Enter the Time "))
+SI = (P*R*T)/100
+print("Simple Interst is " , SI)
+
+#Q2
+
+l=int(input("Enter the length of rectangle"))
+w=int(input("Enter the width of rectangle"))
+print("area of Ractangle is " , l*w)
+
+#Q3
+
+a=int(input("Enter the First number"))
+b=int(input("Enter the First number"))
+c=int(input("Enter the First number"))
+avg=(a+b+c)/3
+print("number of average is ",avg)
+
+#Q4
+
+l=int(input("Enter the length"))
+b=int(input("Enter the width"))
+h=int(input("Enter the height"))
+cuboid=l*b*h
+print("the volume of cuboid is ",cuboid)
+
+#Q5
+
+x=int(input("Enter the First number"))
+y=int(input("Enter the second number"))
+print("muliply the numbers is ",x**y)
