a1d2d1edc2ec7fdb26a87a3891035315d986e6c9
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-20 10:07:11 +0530

    Add files via upload

---
+++ Assign_10.py
@@ -0,0 +1,28 @@
+#Q1
+x=2534//10
+print(x)
+
+#Q2
+x=2089%10
+print(x)
+
+#Q3
+x=int(input("Enter the number"))
+y=int(input("Enter the second number"))
+z=x
+x=y
+y=z
+print("swap is x,y",x,y)
+
+#Q4
+x=int(input("Enter the three Digit number"))
+n=x//10
+y=n//10
+print(y)
+
+
+#Q5
+x=int(input("Enter the three Digit number"))
+n=x//10
+y=n%10
+print(y)
