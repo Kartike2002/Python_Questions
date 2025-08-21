bae1f703b212ef791db2ba66fb144462ab960a2f
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-21 09:35:52 +0530

    Add files via upload

---
+++ Assign_11.py
@@ -0,0 +1,17 @@
+#Q1
+print('my'in input("Enter the string"))
+
+
+#Q2
+s1=input("Enter the  First string")
+s2=input("Enter the  second string")
+print(s1 is s2)
+
+#Q3
+print(5>10<5)
+
+#Q4
+print("Rad" and "White")
+
+#Q5
+print("True" or "False")
