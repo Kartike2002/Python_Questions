cd78c264c8a25bc07ed74cc577f61e832a25a908
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-22 11:13:54 +0530

    Add files via upload

---
+++ Assign_12.py
@@ -0,0 +1,28 @@
+#Q1
+x=int(input("Enter the number"))
+if x>0:
+    print("Postive")
+else:
+    print("Non postive") 
+    
+#Q2
+x=int(input("Enter the number"))
+if x%5==0:
+    print("Diviable")
+else:
+    print("not divided")
+    
+#Q3
+
+x=int(input("Enter the number"))
+if x%2==0:
+    print("Even")
+else:
+    print("odd")
+    
+#Q4
+
+        
+        
+        
+       
