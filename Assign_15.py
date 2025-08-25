59a3e3eb2e7a46cfedc5a85eae78ebd5aae51b58
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-25 11:27:50 +0530

    Add files via upload

---
+++ Assign_15.py
@@ -0,0 +1,35 @@
+#Q1
+i=1
+while i<=5:
+    print("Mysirg")
+    i+=1
+    
+    
+#Q2
+
+i=1
+while i<=10:
+    print("Netural num",i)
+    i+=1
+    
+#Q3
+
+i=10
+while i>=1:
+    print("Revers num",i)
+    i-=1   
+    
+#Q4
+
+i=1
+while i<=10:
+    print(" natural num",2*i-1)
+    i+=1         
+    
+#Q5
+
+i=10
+while i>=1:
+    print("Revers num",2*i-1)
+    i-=1   
+    
