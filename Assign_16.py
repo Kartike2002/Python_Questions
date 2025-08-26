c2446351ea2ba20db9199d1649c0975138b2bf5e
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-26 10:42:51 +0530

    Add files via upload

---
+++ Assign_16.py
@@ -0,0 +1,32 @@
+#Q1
+i=1
+while(i<=10):
+    print("Even num",2*i)
+    i+=1
+#Q2
+
+i=10
+while(i>=1):
+    print("Reverse num",2*i)
+    i-=1
+
+#Q3
+
+i=1
+while(i<=10):
+    print("Square",i*i)
+    i+=1  
+    
+#Q4
+
+i=1
+while(i<=10):
+    print("Cube of ",i*i*i)
+    i+=1  
+          
+#Q5
+
+i=1
+while(i<=10):
+    print("Multipe",5*i)
+    i+=1     
