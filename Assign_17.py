ef34f39b84f6bcdc5e65d21011d76678cadd170b
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-27 09:08:12 +0530

    Add files via upload

---
+++ Assign_17.py
@@ -0,0 +1,39 @@
+#Q1
+n=int(input("Enter the num"))
+i=1
+while(i<=n):
+    print("Mysirg")
+    i+=1
+
+#Q2
+n=int(input("Enter the num"))
+i=1
+while(i<=n):
+    print("Natural num",i)
+    i+=1   
+
+#Q3
+
+n=int(input("Enter the num"))
+i=n
+while(i>=1):
+    print("Natural num",i)
+    i-=1     
+    
+    
+    
+#Q4    
+n=int(input("Enter the num"))
+i=1
+while(i<=n):
+    print("Natural num",2*i-1)
+    i+=1     
+    
+    
+#Q5
+
+n=int(input("Enter the num"))
+i=n
+while(i>=1):
+    print("Natural num",2*i-1)
+    i-=1      
