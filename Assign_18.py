8d036a2ffdd6d6c30bc5cfc80ce448af6096f31c
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-28 10:26:29 +0530

    Add files via upload

---
+++ Assign_18.py
@@ -0,0 +1,38 @@
+#Q1
+n=int(input("Enter the number "))
+i=1
+while(i<=n):
+    print("Natural num",2*i)
+    i+=1
+    
+#Q2
+n=int(input("Enter the number "))
+i=n
+while(i>=1):
+    print("Natural num",2*i)
+    i-=1
+    
+#Q3
+n=int(input("Enter the number "))
+i=1
+while(i<=n):
+    print("Natural num",i*i)
+    i+=1
+
+#Q4
+n=int(input("Enter the number "))
+i=1
+while(i<=n):
+    print("Natural num",i*i*i)
+    i+=1   
+    
+#Q5
+n=int(input("Enter the number "))
+i=1
+while(i<=10):
+    print("Multiple",n*i)
+    i+=1 
+    
+
+
+    
