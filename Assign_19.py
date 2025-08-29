999088a8edd2f1f507a855fb1d8da9035afa587c
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-29 13:17:10 +0530

    Add files via upload

---
+++ Assign_19.py
@@ -0,0 +1,38 @@
+#Q1
+"""ch=input("Enter the string  \n")
+for e in ch:
+   print("Coressponding string",e ,ord(e))
+    
+#Q2
+ch=input("Enter the string  \n")
+for e in ch:
+    if (e) in ('a','e','i','o','u','A','E','U','I','O'):
+        print(e)
+       
+#Q3
+ch=input("Enter the string  \n")
+count = 0
+for e in ch:
+    if e==" " :
+        count+=1
+        print("Total count ",count)
+
+
+"""
+
+#Q4
+ 
+n=int(input("Enter the digit"))
+ 
+
+        
+        
+        
+        
+#Q5
+num=int(input("Enter the Digits \n"))
+count=0
+for e in str(num):
+    if  :
+        
+        
