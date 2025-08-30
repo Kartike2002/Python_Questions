e7492dfd802d7acb10bcc099281df7ebf7c4c20a
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-08-30 11:07:31 +0530

    Add files via upload

---
+++ Assign_20.py
@@ -0,0 +1,28 @@
+# Q1
+for i in range(1, 11):
+    print("Multiple of 5 is", 5*i)
+
+# Q2
+
+n = int(input("Enter the number"))
+for i in range(1, 11):
+    print("Multiple of n is ", i*n)
+# Q3
+n = int(input("Enter the number"))
+m = int(input("Enter the number of times"))
+for i in range(1, m+1):
+    print("Multiple of n is ", i*n)
+
+
+# Q4
+
+user_choice = int(input("Enter the number \n"))
+for i in range(10, 0, -1):
+    print("Multiple of ", user_choice*i)
+
+
+# 5
+
+user_choice = int(input("Enter the First number of Table \n"))
+for i in range(1, 11):
+    print("Multiple of ", user_choice*i)
