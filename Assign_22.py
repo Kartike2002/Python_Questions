e52960a9295cc17c020fd11c51ba23729398a354
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-09-01 10:12:17 +0530

    Add files via upload

---
+++ Assign_22.py
@@ -0,0 +1,39 @@
+#Q1
+
+n=int(input("Enter the number : "))
+s=0
+for i in range(1,n+1):
+    s=s+i
+print(s) 
+
+
+#Q2
+ 
+n=int(input("Enter the number : "))
+s=0
+for i in range(1,n+1):
+    s=s+i**2
+print(s)  
+     
+#Q3
+
+n=int(input("Enter the number : "))
+s=0
+for i in range(1,n+1):
+    s=s+i**3
+print(s)  
+
+#Q4
+n=int(input("Enter the number : "))
+s=0
+for i in range(1,n+1):
+    s=s+2*i-1
+print(s)     
+
+
+#5
+n=int(input("Enter the number : "))
+s=0
+for i in range(1,n+1):
+    s=s+2*i
+print(s) 
