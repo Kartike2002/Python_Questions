e32b5303351caf75d9ab381d1f54c1a2a45d0b09
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-09-02 10:33:22 +0530

    Add files via upload

---
+++ Assign_23.py
@@ -0,0 +1,27 @@
+#Q1
+n=int(input("Enter the number"))
+f=1
+for i in range(n,0,-1) :
+    f=f*i       
+print(f) 
+
+#Q2
+
+
+
+
+
+
+
+
+
+#Q3
+n=input("Enter the number")
+count=0
+for i in n:
+    count=count+int(i)
+print(count)    
+    
+       
+    
+    
