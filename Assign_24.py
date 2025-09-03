9073c19ce996c407588be5f3929b3c48969a015e
Aditya nigam <167546305+Kartike2002@users.noreply.github.com>
2025-09-03 11:57:52 +0530

    Add files via upload

---
+++ Assign_24.py
@@ -0,0 +1,45 @@
+#Q1
+def sum(a,b):
+    c=a+b
+    return c
+
+print(sum(5,7))
+
+#Q2
+
+def area(r):
+    a=2*3.14*r
+    return a
+    
+print(area(5))    
+    
+    
+#Q3
+
+def avg(a,b,c):
+    a=a+b+c/3
+    return a
+
+print(avg(1,2,3))
+
+
+#Q4
+
+def compound(p,r,t):
+    I=p*r*t/100
+    return I 
+
+print(compound(1000,6,1))
+
+
+#Q5
+
+def  volume(l,b,h) :
+    Volume_of_a_cuboid = l * b * h
+    return Volume_of_a_cuboid
+
+print(volume(4,5,7))
+    
+
+    
+    
